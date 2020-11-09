be08beb056588b8e690c0a3a9e39c4bbb6ce5463
Anwar Muhammad <anwarsandvip@gmail.com>
2020-11-09 08:28:22 +0600

    ddd
    ddd

--- like_button.js
+++ like_button.js
@@ -20,5 +20,6 @@ class LikeButton extends React.Component {
   }
 }
 
-const domContainer = document.querySelector('#like_button_container');
-ReactDOM.render(e(LikeButton), domContainer);
+
+
+ReactDOM.render(<LikeButton />, document.getElementById('like_button_container'));
